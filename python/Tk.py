e52f3ed9b0900ca7c1e466aae70e8737aeee471c
Christin Cürten <christin.cuerten@cpi.koeln>
2021-06-19 10:36:07 +0200

    Grundfunktionen  und Projekte installiert. Funktionen in eigener Datei ausgelagert. kann noch weiter strukturiert werden-->Klassen?

--- python/Tk.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-root=tk.Tk()
-eingabefeld=tk.Entry(root)
-eingabefeld_wert=tk.StringVar()
-eingabefeld["textvariable"] = eingabefeld_wert
-eingabefeld["show"] = "*"
-eingabefeld.pack()
